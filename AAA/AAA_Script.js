c3f2c8bb52c128b160f14526c1ed36702cfbfddc
arakanartarea <arakanartarea@gmail.com>
2026-05-26 16:12:07 +0700

    Update AAA_Script.js

--- AAA/AAA_Script.js
+++ AAA/AAA_Script.js
@@ -657,7 +657,7 @@ function closeLoginModal() {
  
  // Login စ 
 // Deploy 1 URL - User အချက်အလက်များ သိမ်းရန်
-const webAppUrl_User = "https://script.google.com/macros/s/AKfycbzXhfr1-EJhp_yEZWzggeXGI3ZPZ32IqV4B_djYUMZthO7afMAfte_4tU7oEr9pz63c/exec"; // ဒီနေရာမှာ Deploy 1 လင့်ခ် ထည့်ပါ
+const webAppUrl_User = "https://script.google.com/macros/s/AKfycbx0DvWcd49am1XalvnhEaSn7IMcNy9t7J6vlECZToARmNyzWNP192WGZJ9AAhd-jJqQ/exec"; // ဒီနေရာမှာ Deploy 1 လင့်ခ် ထည့်ပါ
 
 // Nav Profile အကောင့်ဝင်ရန် နှိပ်သည့်အခါ
 function promptGoogleLogin() {
